e781c88feb8e2a31d02f42ae0b3aaa052a3993cb
d954mas <d954mas@mail.ru>
2019-03-24 18:48:03 +0300

    wip

--- assets/levels/levels.lua
+++ assets/levels/levels.lua
@@ -1,8 +1,8 @@
 local M = {}
 
-M.LVL_1 = require "assets.levels.level_4"
-M.LVL_2 = require "assets.levels.level_6"
-M.LVL_3 = require "assets.levels.level_1"
+M.LVL_1 = require "assets.levels.level_6"
+M.LVL_2 = require "assets.levels.level_1"
+M.LVL_3 = require "assets.levels.level_4"
 M.LVL_4 = require "assets.levels.level_3"
 M.LVL_5 = require "assets.levels.level_5"
 M.LVL_6 = require "assets.levels.level_2"
